99efd6b36213d42fb21a3787d8626fd555563cba
Robin-Shipp <Robin-Shipp@users.noreply.github.com>
2022-11-21 17:06:59 +0000

    deploy: 2e665a4b97948b802d3f5b91ad9626802843b363

---
+++ en.search-data.min.301563cf66cdb5dd0f2f5ea102025362e5127812073eca2dc84cb8ff445f8601.js
@@ -0,0 +1,5 @@
+"use strict";(function(){const e={cache:!0};e.doc={id:"id",field:["title","content"],store:["title","href","section"]};const t=FlexSearch.create("balance",e);window.bookSearchIndex=t,t.add({id:0,href:"/docs/010-introduction/",title:"Introduction",section:"Docs",content:` Introduction # UNDER CONSTRUCTION
+Image Credits # Figure Details Title Picture Disclaimer # Whilst every effort has been made to ensure the accuracy of this information, neither contributors nor the Central Council of Church Bell Ringers can accept responsibility for any inaccuracies or for any activities undertaken based on the information provided.
+Version 0.1 (UNDER CONSTRUCTION), November 2022
+© 2022 Central Council of Church Bell Ringers
+`})})()
