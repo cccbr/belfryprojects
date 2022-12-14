18f06828346c4c8a1cf5397c43a8e170769fa085
Robin-Shipp <Robin-Shipp@users.noreply.github.com>
2022-12-14 18:55:55 +0000

    deploy: feb443d4c37ac0957f5e49e3b9c54f047cbac9e0

--- en.search-data.min.a2df53d554a34f41352f6a603ca46044adf434794456f5d1561acc826ad944a2.js
+++
@@ -1,5 +0,0 @@
-"use strict";(function(){const e={cache:!0};e.doc={id:"id",field:["title","content"],store:["title","href","section"]};const t=FlexSearch.create("balance",e);window.bookSearchIndex=t,t.add({id:0,href:"/docs/",title:"Scoping the Project",section:"Belfry Projects",content:` Scoping the Project # UNDER CONSTRUCTION
-Disclaimer # Whilst every effort has been made to ensure the accuracy of this information, neither contributors nor the Central Council of Church Bell Ringers can accept responsibility for any inaccuracies or for any activities undertaken based on the information provided.
-Version 0.1 (Under construction), December 2022
-© 2022 Central Council of Church Bell Ringers
-`})})()
