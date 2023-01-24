20cd8f489492de6ce0627f662a22ffe4f0a77474
Allen-Git-hub <Allen-Git-hub@users.noreply.github.com>
2023-01-24 16:51:25 +0000

    deploy: 1a6bca2ae0082e1851134c1ec4abf9945e6e05ad

--- en.search-data.min.cce1739409cf1a44dcf3695a071422d7852fcbc02ac9c0614c93f62fda597f9f.js
+++ en.search-data.min.cce1739409cf1a44dcf3695a071422d7852fcbc02ac9c0614c93f62fda597f9f.js
@@ -70,8 +70,8 @@ Version 0.1 (Under construction), December 2022
 © 2022 Central Council of Church Bell Ringers
 `}),e.add({id:2,href:"/docs/030-specialist-advice/",title:"Getting Specialist Advice",section:"Docs",content:` Getting Specialist Advice # UNDER CONSTRUCTION
 Previous Chapter - Next Chapter Disclaimer # Whilst every effort has been made to ensure the accuracy of this information, neither contributors nor the Central Council of Church Bell Ringers can accept responsibility for any inaccuracies or for any activities undertaken based on the information provided.
-Version 0.1 (Under construction), December 2022
-© 2022 Central Council of Church Bell Ringers
+Version 0.1 (Under construction), January 2023
+© 2023 Central Council of Church Bell Ringers
 `}),e.add({id:3,href:"/docs/040-managing-project/",title:"Managing the Project",section:"Docs",content:` Managing the Project # A plan to go from this… # Picture here
 As a project takes shape there may be enthusiasm to make progress, particularly start raising money. However, it is advisable to plan the project, considering in some detail what has to be done when, how, by whom, with what resources first. The plan will ideally run from the earliest discussions through to project completion. Careful planning should reduce the overall duration, save money and make sure that the whole project runs smoothly and meets its aims.
 Asking who, what, when, where, why, how, are useful prompts at every stage! There is a wide range of material available about planning and running projects. Look for basic guides online and in books (public libraries and bookstalls at larger stations may stock some). A focus on charities and the voluntary sector is most likely to be helpful for bell projects. There is no “right” way to manage a project so this guidance should be adjusted to fit the specific project.
