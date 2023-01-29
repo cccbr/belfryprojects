6f2a51fe1d9d66be5d4a49e7def6b879c4f9ba7d
Robin-Shipp <Robin-Shipp@users.noreply.github.com>
2023-01-29 10:43:26 +0000

    deploy: ae09b752fb19a5a26794f8473a71242c79c6016f

--- en.search-data.min.17f132cfedbcd9b7123ff30912ead02fa710e6fed78f8c76b1b501b2b4f7c935.js
+++ en.search-data.min.17f132cfedbcd9b7123ff30912ead02fa710e6fed78f8c76b1b501b2b4f7c935.js
@@ -70,6 +70,9 @@ Version 0.1 (Under construction), December 2022
 © 2022 Central Council of Church Bell Ringers
 `}),e.add({id:2,href:"/docs/030-specialist-advice/",title:"Getting Specialist Advice",section:"Docs",content:` Getting Specialist Advice # UNDER CONSTRUCTION
 Test of Specialist List # Info and contact details about Taylors
+More info
+More info
+More info
 Previous Chapter - Next Chapter Disclaimer # Whilst every effort has been made to ensure the accuracy of this information, neither contributors nor the Central Council of Church Bell Ringers can accept responsibility for any inaccuracies or for any activities undertaken based on the information provided.
 Version 0.1 (Under construction), January 2023
 © 2023 Central Council of Church Bell Ringers
