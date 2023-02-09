f15132a37e254e2c9a02877642f00d7a5766a6ee
Robin-Shipp <Robin-Shipp@users.noreply.github.com>
2023-02-09 19:58:36 +0000

    deploy: 8b975f861ac636ba061158afd99e18ae66a2e29b

--- en.search-data.min.b72ee12d9ce18fbe09365e24a236f3cf7c289fa82cd9d915bd292251b685fcb8.js
+++ en.search-data.min.b72ee12d9ce18fbe09365e24a236f3cf7c289fa82cd9d915bd292251b685fcb8.js
@@ -134,6 +134,8 @@ Previous Chapter - Next Chapter Disclaimer # Whilst every effort has been made t
 Version 0.1 (Under construction), January 2023
 © 2023 Central Council of Church Bell Ringers
 `}),e.add({id:4,href:"/docs/050-project-team/",title:"Project Team",section:"Docs",content:` Project Team # UNDER CONSTRUCTION
+Appointing the Project Team # Team working # The team should discuss and agree how they intend to operate, share workload and activities, distribute information and maintain records. The team will need to meet periodically to discuss and agree key matters, supplemented by frequent electronic communications.
+As team members get to know each other, they will build a rapport enabling them to work together through the project. There will be high and low points, frustrations and successes; socialising together will help!
 Previous Chapter - Next Chapter Disclaimer # Whilst every effort has been made to ensure the accuracy of this information, neither contributors nor the Central Council of Church Bell Ringers can accept responsibility for any inaccuracies or for any activities undertaken based on the information provided.
 Version 0.1 (Under construction), January 2023
 © 2023 Central Council of Church Bell Ringers
